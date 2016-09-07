7886905782b3a69051f7530ba34005efb49066ad
door1313 <door13131@gmail.com>
2016-09-07 23:06:16 +0800

    add stream testcase

--- src/wayne/test/MyTest.java
+++ src/wayne/test/MyTest.java
@@ -1,5 +1,13 @@
 package wayne.test;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class MyTest {
 
+	public static void main(String... arg){
+		Optional<Integer> max = Arrays.asList(1,9,3,4,8).stream().map(i -> i+1).max(Integer::compareTo);
+		System.out.println(max.get());
+	}
+	
 }
